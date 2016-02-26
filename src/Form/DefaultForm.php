21ee9a1c17461c7e549cd0d7f7c5ef8b3104dd85
Florian Latzel <florian.latzel@reinblau.de>
2016-02-26 11:18:11 +0100

    Optimized default form removed switch statement.

--- src/Form/DefaultForm.php
+++ src/Form/DefaultForm.php
@@ -22,7 +22,7 @@ class DefaultForm extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'akv_pagepeels.default_config'
+      'akv_pagepeels.settings'
     ];
   }
 
@@ -37,22 +37,22 @@ class DefaultForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('akv_pagepeels.default_config');
+    $config = $this->config('akv_pagepeels.settings');
     $form['akv_pagepeels_type'] = array(
       '#type' => 'radios',
       '#required' => true,
       '#title' => $this->t('Pagepeel type'),
       '#description' => $this->t('Choose the type of collapsed pagepeel'),
       '#options' => array(
-        'latest' => $this->t('Latest pagepeel from AKV'),
+        'akv_pagepeels_latest' => $this->t('Latest pagepeel from AKV'),
         // http://wiki.vorratsdatenspeicherung.de/images/archive/20101110133306%21Akvse.gif
-        'de_maiziere' => $this->t('Thomas de Maizière'),
-        'schaeuble' => $this->t('Stasi 2.0 classic (Schäublone)'),
-        'schaeuble_green' => $this->t('Stasi 2.0 green (Schäublone)'),
-        'cam' => $this->t('Surveilance camera(2D)'),
-        '3d_cam' => $this->t('Surveilance camera(3D)'),
-        'ani_cam' => $this->t('Surveilance camera(3D + animated)'),
-        'trauer' => $this->t('Condolement, 1949 - 2007 t'),
+        //'de_maiziere' => $this->t('Thomas de Maizière'),
+        'akv_pagepeels_schaeuble' => $this->t('Stasi 2.0 classic (Schäublone)'),
+        'akv_pagepeels_schaeuble_green' => $this->t('Stasi 2.0 green (Schäublone)'),
+        'akv_pagepeels_cam' => $this->t('Surveilance camera(2D)'),
+        'akv_pagepeels_3d_cam' => $this->t('Surveilance camera(3D)'),
+        'akv_pagepeels_ani_cam' => $this->t('Surveilance camera(3D + animated)'),
+        'akv_pagepeels_trauer' => $this->t('Condolement, 1949 - 2007 t'),
       ),
       '#default_value' => $config->get('akv_pagepeels_type'),
     );
@@ -62,8 +62,8 @@ class DefaultForm extends ConfigFormBase {
       '#title' => $this->t('Show info'),
       '#description' => t('Show info icon under the pagepeel'),
       '#options' => array(
-        'on' => $this->t('Show'),
-        'off' => $this->t('Don´t show')
+        'akv_pagepeels' => $this->t('Show'),
+        'akv_pagepeels_no_info' => $this->t('Don´t show')
       ),
       '#default_value' => $config->get('akv_pagepeels_info'),
     );
@@ -81,34 +81,10 @@ class DefaultForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-
-    switch ($form_state->getValue('akv_pagepeels_type')) {
-      case "latest":
-        $akv_css = '/css/akv_pagepeels_latest.css';
-        break;
-      case "schaeuble":
-        variable_set('akv_css', '/css/akv_pagepeels_schaeuble.css');
-        break;
-      case "schaeuble_green":
-        variable_set('akv_css', '/css/akv_pagepeels_schaeuble_green.css');
-        break;
-      case "cam":
-        variable_set('akv_css', '/css/akv_pagepeels_cam.css');
-        break;
-      case "3d_cam":
-        variable_set('akv_css', '/css/akv_pagepeels_3d_cam.css');
-        break;
-      case "ani_cam":
-        variable_set('akv_css', '/css/akv_pagepeels_ani_cam.css');
-        break;
-      case "trauer":
-        variable_set('akv_css', '/css/akv_pagepeels_trauer.css');
-    }
-
-    $this->config('akv_pagepeels.default_config')
+    $this->config('akv_pagepeels.settings')
       ->set('akv_pagepeels_type', $form_state->getValue('akv_pagepeels_type'))
-      ->set('akv_pagepeels_info', $form_state->getValue('akv_pagepeels_info   '))
+      ->set('akv_pagepeels_info', $form_state->getValue('akv_pagepeels_info'))
       ->save();
+    parent::submitForm($form, $form_state);
   }
 }
